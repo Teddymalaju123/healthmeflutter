5779f99b126f52d0abe4e264bdacdfedaefd5b1b
flame123flame <flame.tana@gmail.com>
2023-03-28 22:33:00 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapphealthme/src/features/authentication/login.dart';
+import 'package:flutterapphealthme/src/features/fitness_app/fitness_app_home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +27,19 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: Login());
+        home: FitnessAppHomeScreen());
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
   }
 }
 
